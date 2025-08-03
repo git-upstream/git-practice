057e6a156f3650ee28752758a27966228af15c5e
Don Mallow <donmallow@Dons-Mac-mini.local>
2025-08-03 16:53:35 -0400

    add textfield for the notes entry
    1) webview in separate window opened with main menu button
    
    2) window group manager added to environment
    
    3) add webview window and move enum out of windowgroupmanager

--- GitPractice/ChapterDetailView.swift
+++ GitPractice/ChapterDetailView.swift
@@ -2,8 +2,11 @@ import SwiftUI
 import WebKit
 
 struct ChapterDetailView: View {
-    let swiftUIDayModel: SwiftUIDayModel
+    @Environment(\.openWindow) private var openWindow
 
+    let swiftUIDayModel: SwiftUIDayModel
+    @State private var notes = ""
+    
     var body: some View {
         VStack {
             Text("Chapter \(swiftUIDayModel.number) Detail View")
@@ -11,8 +14,17 @@ struct ChapterDetailView: View {
             Text(String(swiftUIDayModel.isCompleted))
             Text(swiftUIDayModel.notes)
             
-            WebView(url: swiftUIDayModel.url)
-
+            
+                ScrollView {
+                    TextField("Add notes...", text: $notes)
+                        .padding()
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                }
+                .background(.gray.opacity(0.4))
+                
+            Button("Open Website") {
+                openWindow(id: "webview")
+            }
         }
     }
 }
---
+++ GitPractice/WindowGroupManager.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@Observable
+class WindowGroupManager {
+    var currentWindowGroup: WindowGroupType = .mainMenu
+}
+
+enum WindowGroupType: String {
+    case mainMenu = "mainMenu"
+    case webview = "webview"
+}
