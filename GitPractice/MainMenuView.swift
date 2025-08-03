2f9074c3590541b7bd9f959c7f102885058f835e
Git Upstream <gitupstream@gmail.com>
2025-08-03 12:09:02 -0400

    rename contentview mainview
    remove original main view and rename contentview to mainmenuview

--- GitPractice/MainMenuView.swift
+++ GitPractice/MainMenuView.swift
@@ -1,13 +1,31 @@
 import SwiftUI
+import RealityKit
+import RealityKitContent
 
 struct MainMenuView: View {
+    @State private var listOfChapters: [String] = ["Chapter 1"]
+    @State private var selectedChapter: String? = nil
+
     var body: some View {
-        VStack {
-            Text("Main Menu")
+        NavigationStack {
+            VStack {
+                Text("GitHub Practice")
+                    .font(.extraLargeTitle)
+                
+                Text("100 Days of SwiftUI")
+                    .font(.title)
+                
+                Text("You will be doing one chapter a day for 100 days.")
+                
+                List(listOfChapters, id: \.self, selection: $selectedChapter) { chapter in
+                    Text(chapter)
+                }
+            }
+            .padding()
         }
     }
 }
 
-#Preview {
+#Preview(windowStyle: .automatic) {
     MainMenuView()
 }
