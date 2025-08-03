4ef2defa183650615b7f1e8db2634e5de322de5e
Git Upstream <gitupstream@gmail.com>
2025-08-03 08:26:21 -0400

    add default windowgroup size
    1280 x 720

--- GitPractice/GitPracticeApp.swift
+++ GitPractice/GitPracticeApp.swift
@@ -12,6 +12,7 @@ struct GitPracticeApp: App {
         WindowGroup {
             MainMenuView()
         }
+        .defaultSize(width: 1280, height: 1280)
     }
 }
 // MARK: This is the first comment as a team member for git-collaborator
