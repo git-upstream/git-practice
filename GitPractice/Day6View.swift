77c46b2180614f41c61fc34593465468f0f9b565
Git Upstream <gitupstream@gmail.com>
2025-08-12 16:08:05 -0400

    add chapter 6

--- GitPractice/Day6View.swift
+++ GitPractice/Day6View.swift
@@ -1,18 +1,44 @@
-//
-//  Day6View.swift
-//  GitPractice
-//
-//  Created by Don Mallow on 8/10/25.
-//
+/*
+// for in loops discussed
+ for os in platforms {
+     print("Swift works great on \(os).")
+ }
+ for i in 1...12 {
+     print("5 x \(i) is \(5 * i)")
+ }
 
-import SwiftUI
+ nested loops:
+ for i in 1...12 {
+     print("The \(i) times table:")
+     for j in 1...12 {
+         print("  \(j) x \(i) is \(j * i)")
+     }
+     print()
+ }
 
-struct Day6View: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+while loops:
+ var countdown = 10
+ while countdown > 0 {
+     print("\(countdown)…")
+     countdown -= 1
+ }
+ print("Blast off!")
 
-#Preview {
-    Day6View()
-}
+ continue and break statements in loops
+
+FizzBuzz solution
+ for num in 1...100 {
+     if num % 15 == 0 {
+         print("FizzBuzz")
+     } else if num % 3 == 0 {
+         print("Fizz")
+     } else if num % 5 == 0 {
+         print("Buzz")
+     } else {
+         print(i)
+     }
+ }
+
+
+
+ */
